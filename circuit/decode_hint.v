043009bf216321189fe1c5ccb3845373e689ead5
William Woodruff <william@trailofbits.com>
2021-03-11 10:48:18 -0500

    tiny86, circuit: memory hint decoding

---
+++ circuit/decode_hint.v
@@ -0,0 +1,16 @@
+module decode_hint(
+  input [71:0] raw_hint,
+
+  output [1:0] mask,
+  output rw,
+  output [31:0] address,
+  output [31:0] data
+);
+
+assign mask = raw_hint[1:0];
+assign rw = raw_hint[2];
+
+assign address = raw_hint[39:8];
+assign data = raw_hint[71:40];
+
+endmodule
