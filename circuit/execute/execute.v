3288565e4a6ecb36da0a2433f43ac15d51e54a12
William Woodruff <william@trailofbits.com>
2021-03-25 17:57:24 -0400

    circuit/execute: plugging away

--- circuit/execute/execute.v
+++ circuit/execute/execute.v
@@ -9,7 +9,8 @@ module execute(
 wire [63:0] opc_1hot = one_hot64(opc);
 
 // ALU control signals.
-// TODO(ww): Invert signal for ADD op with CMD_SUB
+
+// Core operation signals.
 wire alu_add = opc_1hot[`CMD_ADD]
                | opc_1hot[`CMD_ADC] | opc_1hot[`CMD_SUB]
                | opc_1hot[`CMD_SBB] | opc_1hot[`CMD_INC]
@@ -19,5 +20,8 @@ wire alu_or = opc_1hot[`CMD_OR];
 wire alu_xor = opc_1hot[`CMD_XOR];
 wire alu_mul = opc_1hot[`CMD_MUL] | opc_1hot[`CMD_IMUL];
 wire alu_div = opc_1hot[`CMD_DIV] | opc_1hot[`CMD_IDIV];
+
+// Auxiliary signals.
+wire alu_src_inv = opc_1hot[`CMD_SUB] | opc_1hot[`CMD_SBB] | opc_1hot[`CMD_DEC];
 
 endmodule
