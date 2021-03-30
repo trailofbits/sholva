65b0da9d4bfa5aec27f2d973309c4e1f039a0db3
William Woodruff <william@trailofbits.com>
2021-03-30 15:17:51 -0400

    circuit/execute: more op mapping, cleanup

--- circuit/execute/execute.v
+++ circuit/execute/execute.v
@@ -17,19 +17,35 @@ wire [63:0] opc_1hot = one_hot64(opc);
 // ALU control signals.
 
 // Core operation signals.
-wire alu_op_add = opc_1hot[`CMD_ADD] | opc_1hot[`CMD_ADC] | opc_1hot[`CMD_INC];
-wire alu_op_sub = opc_1hot[`CMD_SUB] | opc_1hot[`CMD_SBB] | opc_1hot[`CMD_DEC];
-wire alu_op_and = opc_1hot[`CMD_AND];
+wire alu_op_add = opc_1hot[`CMD_ADD] |
+                  opc_1hot[`CMD_ADC] |
+                  opc_1hot[`CMD_INC];
+
+wire alu_op_sub = opc_1hot[`CMD_SUB] |
+                  opc_1hot[`CMD_SBB] |
+                  opc_1hot[`CMD_DEC] |
+                  opc_1hot[`CMD_CMP] |
+                  opc_1hot[`CMD_CMPS];
+
+wire alu_op_and = opc_1hot[`CMD_AND] |
+                  opc_1hot[`CMD_TEST];
+
 wire alu_op_or = opc_1hot[`CMD_OR];
-wire alu_op_xor = opc_1hot[`CMD_XOR] | opc_1hot[`CMD_NOT];
-wire alu_op_mul = opc_1hot[`CMD_MUL] | opc_1hot[`CMD_IMUL];
-wire alu_op_div = opc_1hot[`CMD_DIV] | opc_1hot[`CMD_IDIV];
+
+wire alu_op_xor = opc_1hot[`CMD_XOR] |
+                  opc_1hot[`CMD_NOT];
+
+wire alu_op_mul = opc_1hot[`CMD_MUL] |
+                  opc_1hot[`CMD_IMUL];
+
+wire alu_op_div = opc_1hot[`CMD_DIV] |
+                  opc_1hot[`CMD_IDIV];
 
 // Auxiliary signals.
 wire alu_src_inv = opc_1hot[`CMD_SUB] | opc_1hot[`CMD_SBB] | opc_1hot[`CMD_DEC];
 wire alu_use_carry = opc_1hot[`CMD_ADC] | opc_1hot[`CMD_SBB];
 wire alu_src_inc = opc_1hot[`CMD_SUB] | opc_1hot[`CMD_SBB];
-wire alu_no_wr = opc_1hot[`CMD_CMP] | opc_1hot[`CMD_CMPS];
+wire alu_no_wr = opc_1hot[`CMD_CMP] | opc_1hot[`CMD_CMPS] | opc_1hot[`CMD_TEST];
 wire alu_no_flags = opc_1hot[`CMD_NOT];
 // TODO(ww): Flesh these out more:
 // https://sandpile.org/x86/flags.htm
