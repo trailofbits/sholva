2c022dbe6ee92023afa88ff21c70b1f9344f7fa4
William Woodruff <william@trailofbits.com>
2021-04-05 18:24:40 -0400

    circuit: one_hot8, use hot8 form for regfile

--- circuit/include/funcs.v
+++ circuit/include/funcs.v
@@ -6,3 +6,11 @@ function [63:0] one_hot64;
       one_hot64[binary] =  1'b1;
    end
 endfunction
+
+function [7:0] one_hot8;
+  input [2:0] binary;
+  begin
+    one_hot8 = 8'h00;
+    one_hot8[binary] = 1'b1;
+  end
+endfunction
--- circuit/regfile.v
+++ circuit/regfile.v
@@ -19,31 +19,35 @@ module regfile(
   output [31:0] o_eax, o_ebx, o_ecx, o_edx, o_esi, o_edi, o_esp, o_ebp, o_eip, o_eflags
 );
 
+`include "funcs.v"
+
+wire [7:0] dest0_sel_1hot = one_hot8(dest0_sel);
+wire [7:0] dest1_sel_1hot = one_hot8(dest1_sel);
+
 // Ugly.
-// TODO(ww): Maybe use a 1hot form for the selectors here, to avoid lots of 3-bit compares.
-assign o_eax = (dest0_kind[`OPND_DEST_REG] && dest0_sel == `REG_EAX) ? opnd0_w :
-               (dest1_kind[`OPND_DEST_REG] && dest1_sel == `REG_EAX) ? opnd1_w : i_eax;
+assign o_eax = (dest0_kind[`OPND_DEST_REG] && dest0_sel_1hot[`REG_EAX]) ? opnd0_w :
+               (dest1_kind[`OPND_DEST_REG] && dest1_sel_1hot[`REG_EAX]) ? opnd1_w : i_eax;
 
-assign o_ebx = (dest0_kind[`OPND_DEST_REG] && dest0_sel == `REG_EBX) ? opnd0_w :
-               (dest1_kind[`OPND_DEST_REG] && dest1_sel == `REG_EBX) ? opnd1_w : i_ebx;
+assign o_ebx = (dest0_kind[`OPND_DEST_REG] && dest0_sel_1hot[`REG_EBX]) ? opnd0_w :
+               (dest1_kind[`OPND_DEST_REG] && dest1_sel_1hot[`REG_EBX]) ? opnd1_w : i_ebx;
 
-assign o_ecx = (dest0_kind[`OPND_DEST_REG] && dest0_sel == `REG_ECX) ? opnd0_w :
-               (dest1_kind[`OPND_DEST_REG] && dest1_sel == `REG_ECX) ? opnd1_w : i_ecx;
+assign o_ecx = (dest0_kind[`OPND_DEST_REG] && dest0_sel_1hot[`REG_ECX]) ? opnd0_w :
+               (dest1_kind[`OPND_DEST_REG] && dest1_sel_1hot[`REG_ECX]) ? opnd1_w : i_ecx;
 
-assign o_edx = (dest0_kind[`OPND_DEST_REG] && dest0_sel == `REG_EDX) ? opnd0_w :
-               (dest1_kind[`OPND_DEST_REG] && dest1_sel == `REG_EDX) ? opnd1_w : i_edx;
+assign o_edx = (dest0_kind[`OPND_DEST_REG] && dest0_sel_1hot[`REG_EDX]) ? opnd0_w :
+               (dest1_kind[`OPND_DEST_REG] && dest1_sel_1hot[`REG_EDX]) ? opnd1_w : i_edx;
 
-assign o_esi = (dest0_kind[`OPND_DEST_REG] && dest0_sel == `REG_ESI) ? opnd0_w :
-               (dest1_kind[`OPND_DEST_REG] && dest1_sel == `REG_ESI) ? opnd1_w : i_esi;
+assign o_esi = (dest0_kind[`OPND_DEST_REG] && dest0_sel_1hot[`REG_ESI]) ? opnd0_w :
+               (dest1_kind[`OPND_DEST_REG] && dest1_sel_1hot[`REG_ESI]) ? opnd1_w : i_esi;
 
-assign o_edi = (dest0_kind[`OPND_DEST_REG] && dest0_sel == `REG_EDI) ? opnd0_w :
-               (dest1_kind[`OPND_DEST_REG] && dest1_sel == `REG_EDI) ? opnd1_w : i_edi;
+assign o_edi = (dest0_kind[`OPND_DEST_REG] && dest0_sel_1hot[`REG_EDI]) ? opnd0_w :
+               (dest1_kind[`OPND_DEST_REG] && dest1_sel_1hot[`REG_EDI]) ? opnd1_w : i_edi;
 
-assign o_esp = (dest0_kind[`OPND_DEST_REG] && dest0_sel == `REG_ESP) ? opnd0_w :
-               (dest1_kind[`OPND_DEST_REG] && dest1_sel == `REG_ESP) ? opnd1_w : i_esp;
+assign o_esp = (dest0_kind[`OPND_DEST_REG] && dest0_sel_1hot[`REG_ESP]) ? opnd0_w :
+               (dest1_kind[`OPND_DEST_REG] && dest1_sel_1hot[`REG_ESP]) ? opnd1_w : i_esp;
 
-assign o_ebp = (dest0_kind[`OPND_DEST_REG] && dest0_sel == `REG_EBP) ? opnd0_w :
-               (dest1_kind[`OPND_DEST_REG] && dest1_sel == `REG_EBP) ? opnd1_w : i_ebp;
+assign o_ebp = (dest0_kind[`OPND_DEST_REG] && dest0_sel_1hot[`REG_EBP]) ? opnd0_w :
+               (dest1_kind[`OPND_DEST_REG] && dest1_sel_1hot[`REG_EBP]) ? opnd1_w : i_ebp;
 
 assign o_eip = next_eip;
 assign o_eflags = next_eflags;
