5ad5787d5aca413db64587c8fbcec4dfa4146fed
William Woodruff <william@trailofbits.com>
2021-06-10 16:42:05 -0400

    circuit/decode: lint fixes

--- circuit/decode/decode_opnd_signals.v
+++ circuit/decode/decode_opnd_signals.v
@@ -29,7 +29,7 @@ wire [15:0] opnd_form_1hot = one_hot16(opnd_form);
 wire [63:0] opc_1hot = one_hot64(opc);
 
 // Whether we have any immediate byte(s).
-wire has_imm = opnd_form_1hot[`OPND_ENC_IMM] ||
+assign has_imm = opnd_form_1hot[`OPND_ENC_IMM] ||
                opnd_form_1hot[`OPND_ENC_MODREGRM_RM_IMM] ||
                opnd_form_1hot[`OPND_ENC_REG_IMM] ||
                opnd_form_1hot[`OPND_ENC_EAX_IMM] ||
@@ -37,7 +37,7 @@ wire has_imm = opnd_form_1hot[`OPND_ENC_IMM] ||
                opnd_form_1hot[`OPND_ENC_MODREGRM_RM_REG_IMM];
 
 // ModR/M encodings where R/M is operand#0 (i.e. op.d=0)
-wire opnd0_modrm_rm = opnd_form_1hot[`OPND_ENC_MODREGRM_RM] ||
+assign opnd0_modrm_rm = opnd_form_1hot[`OPND_ENC_MODREGRM_RM] ||
                       opnd_form_1hot[`OPND_ENC_MODREGRM_RM_IMM] ||
                       opnd_form_1hot[`OPND_ENC_MODREGRM_RM_REG] ||
                       opnd_form_1hot[`OPND_ENC_MODREGRM_RM_REG_IMM] ||
@@ -45,39 +45,39 @@ wire opnd0_modrm_rm = opnd_form_1hot[`OPND_ENC_MODREGRM_RM] ||
 
 
 // ModR/M encodings where REG is operand#0 (i.e. op.d=1)
-wire opnd0_modrm_reg = opnd_form_1hot[`OPND_ENC_MODREGRM_REG_RM] ||
+assign opnd0_modrm_reg = opnd_form_1hot[`OPND_ENC_MODREGRM_REG_RM] ||
                        opnd_form_1hot[`OPND_ENC_MODREGRM_REG_RM_IMM];
 
 
 // ModR/M encodings where R/M is operand#1.
-wire opnd1_modrm_rm = opnd0_modrm_reg;
+assign opnd1_modrm_rm = opnd0_modrm_reg;
 
 // ModR/M encodings where REG is operand#1.
-wire opnd1_modrm_reg = opnd_form_1hot[`OPND_ENC_MODREGRM_RM_REG] ||
+assign opnd1_modrm_reg = opnd_form_1hot[`OPND_ENC_MODREGRM_RM_REG] ||
                        opnd_form_1hot[`OPND_ENC_MODREGRM_RM_REG_IMM] ||
                        opnd_form_1hot[`OPND_ENC_MODREGRM_RM_REG_CL];
 
 // Whether we have a ModR/M byte supplying one or more operands.
-wire has_modrm = opnd0_modrm_rm || opnd0_modrm_reg;
+assign has_modrm = opnd0_modrm_rm || opnd0_modrm_reg;
 
 // The actual ModR/M byte, if `has_modrm`.
-wire [7:0] modrm = unescaped_instr[15:8];
+assign modrm = unescaped_instr[15:8];
 
 // Whether ModR/M.rm indicates a register, i.e. "register direct".
-wire modrm_rm_is_reg_direct = modrm[7:6] == 2'b11;
+assign modrm_rm_is_reg_direct = modrm[7:6] == 2'b11;
 
 // Intel SDM Vol. 2A Table 2-1/2-2/2-3: the SIB byte is only present when all
 // of the following conditions hold:
 //  * The ModR/M byte is present;
 //  * We are not performing a 16-bit addressing operation;
 //  * We are not in register direct mode and ModR/M.rm == 0b100
-wire has_sib = has_modrm
+assign has_sib = has_modrm
                && ~prefix_address_16bit
                && ~modrm_rm_is_reg_direct
                && modrm[2:0] == 3'b100;
 
 // The actual SIB byte, if `has_sib`.
-wire [7:0] sib = unescaped_instr[23:16];
+assign sib = unescaped_instr[23:16];
 
 // Whether we have displacement byte(s).
 // Displacement byte(s) are present in two cases:
@@ -89,7 +89,7 @@ wire [7:0] sib = unescaped_instr[23:16];
 //   * We are in an indirect + displacement addressing mode (ModR/M.mod == 0b01 or 0b10)
 // Second, when we are in a displacement-only encoding (i.e., no ModR/M whatsoever).
 // TODO(ww): Handle that second case.
-wire has_disp = (has_modrm
+assign has_disp = (has_modrm
                    && ~modrm_rm_is_reg_direct
                    && ((modrm[2:0] == 3'b101 && modrm[7:6] == 2'b00)
                        || (modrm[7:6] == 2'b01 || modrm[7:6] == 2'b10)))
