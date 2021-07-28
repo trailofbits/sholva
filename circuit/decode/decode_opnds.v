f50db9b9c0c7f74a8cc1f4f3172158470acf86db
William Woodruff <william.woodruff@trailofbits.com>
2021-07-28 12:54:06 -0400

    circuit: signal groundwork for disp routing (#34)
    * circuit: signal groundwork for disp routing
    
    * circuit: disp routing

--- circuit/decode/decode_opnds.v
+++ circuit/decode/decode_opnds.v
@@ -53,8 +53,10 @@ wire modrm_rm_is_reg_direct;
 
 wire opnd0_modrm_rm;
 wire opnd0_modrm_reg;
+wire opnd0_disp;
 wire opnd1_modrm_rm;
 wire opnd1_modrm_reg;
+wire opnd1_disp;
 
 decode_opnd_signals decode_opnd_signals_x(
   // Inputs
@@ -78,8 +80,10 @@ decode_opnd_signals decode_opnd_signals_x(
 
   .opnd0_modrm_rm(opnd0_modrm_rm),
   .opnd0_modrm_reg(opnd0_modrm_reg),
+  .opnd0_disp(opnd0_disp),
   .opnd1_modrm_rm(opnd1_modrm_rm),
-  .opnd1_modrm_reg(opnd1_modrm_reg)
+  .opnd1_modrm_reg(opnd1_modrm_reg),
+  .opnd1_disp(opnd1_disp)
 );
 
 wire [15:0] opnd_form_1hot = one_hot16(opnd_form);
@@ -258,11 +262,8 @@ wire [2:0] opnd1_r_mem_base_regsel =
   (opc_1hot[`CMD_CMPS]) ?
     `REG_EDI : opndX_r_mem_base_regsel;
 
-
-// TODO(ww): Route disp into one of these, depending on which operand
-// needs it.
-wire [31:0] opnd0_r_mem_disp = 32'b0;
-wire [31:0] opnd1_r_mem_disp = 32'b0;
+wire [31:0] opnd0_r_mem_disp = opnd0_disp ? disp : 32'b0;
+wire [31:0] opnd1_r_mem_disp = opnd1_disp ? disp : 32'b0;
 
 wire [31:0] opnd0_r_mem_selected_index;
 mux8_32 mux8_32_opnd0_mem_index(
