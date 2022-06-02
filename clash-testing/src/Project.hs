35456e68f97285dfaf59dead3c3c4ea1f2f7a8a3
jleightcap <jack@leightcap.com>
2022-06-02 12:32:26 -0400

    just kidding, have to ALU_NO_WR external to ALU

--- clash-testing/src/Project.hs
+++ clash-testing/src/Project.hs
@@ -52,35 +52,29 @@ type ControlWord = Vec 18 Bit
 
 type StatusWord = Vec 7 Bit
 
-alu :: Bit -> Register -> Register -> Bit -> ControlWord -> AluResult
-alu no_wr x y carry cntl
-  -- FIXME(jl) how does verilog behave with truncations?
+alu :: Register -> Register -> Bit -> ControlWord -> AluResult
+alu x y carry cntl
+  -- FIXME(jl) reference 80386 semantics for arithmetic ops' truncation behavior.
   -- ex for multiply, clash provides
   --    times :: Vec n -> Vec m -> Vec (n + m)
   --    (*)   :: Vec n -> Vec n -> Vec n
-  -- FIXME(jl) shifting
- =
-  if bitToBool no_wr
-    then 0
-    else go
+  -- so, should this `zeroExend *`, or `truncate times`?
+  | op ALU_OP_ADD = (x `add` y) + (carryVal carry)
+  | op ALU_OP_SUB = (x `sub` y) + (carryVal carry)
+  | op ALU_OP_AND = zeroExtend $ x .&. y
+  | op ALU_OP_OR = zeroExtend $ x .|. y
+  | op ALU_OP_XOR = zeroExtend $ x `xor` y
+  | op ALU_OP_MUL = zeroExtend $ x * y
+  | op ALU_OP_SHL = zeroExtend $ x `shiftL` shiftVal y
+  | op ALU_OP_SHR = zeroExtend $ x `shiftR` shiftVal y
+  | op ALU_OP_ROL = zeroExtend $ x `rotateL` shiftVal y
+  | op ALU_OP_ROR = zeroExtend $ x `rotateR` shiftVal y
+  | op ALU_OP_DIV = zeroExtend $ x `div` y
+  | otherwise = undefined -- all invocations of ALU must set _at least one_ of the operation bits.
   where
-    go
-      | op ALU_OP_ADD = (x `add` y) + (carryVal carry)
-      | op ALU_OP_SUB = (x `sub` y) + (carryVal carry)
-      | op ALU_OP_AND = zeroExtend $ x .&. y
-      | op ALU_OP_OR = zeroExtend $ x .|. y
-      | op ALU_OP_XOR = zeroExtend $ x `xor` y
-      | op ALU_OP_MUL = zeroExtend $ x * y
-      | op ALU_OP_SHL = zeroExtend $ x `shiftL` shiftVal y
-      | op ALU_OP_SHR = zeroExtend $ x `shiftR` shiftVal y
-      | op ALU_OP_ROL = zeroExtend $ x `rotateL` shiftVal y
-      | op ALU_OP_ROR = zeroExtend $ x `rotateR` shiftVal y
-      | op ALU_OP_DIV = zeroExtend $ x `div` y
-      | otherwise = undefined -- all invocations of ALU must set _at least one_ of the operation bits.
-      where
-        carryVal = zeroExtend . pack
-        shiftVal = unpack . resize
-        op = (1 ==) . (cntl !!>)
+    carryVal = zeroExtend . pack
+    shiftVal = unpack . resize
+    op = (1 ==) . (cntl !!>)
 
 status ::
      StatusWord
@@ -145,12 +139,11 @@ topEntity (cntl, status_in, opnd0_r, opnd1_r) = (status_out, result)
     carry_in :: Signal System Bit
     carry_in = ((!!> ALU_USE_CARRY) <$> cntl) * ((!!> STAT_CF) <$> status_in)
     stat_result :: Signal System (Vec 33 Bit)
-    stat_result =
-      unpack <$> (alu <$> alu_no_wr <*> op0 <*> op1 <*> carry_in <*> cntl)
-      where
-        alu_no_wr = (!!> ALU_NO_WR) <$> cntl
+    stat_result = unpack <$> (alu <$> op0 <*> op1 <*> carry_in <*> cntl)
     result :: Signal System (Vec 32 Bit)
-    result = tail <$> stat_result
+    result = mux alu_no_wr (pure $ unpack 0) (tail <$> stat_result)
+      where
+        alu_no_wr = bitToBool <$> (!!> ALU_NO_WR) <$> cntl
     status_out :: Signal System (Vec 7 Bit)
     status_out =
       status <$> status_in <*> opnd0_r <*> opnd1_r <*> stat_result <*> cntl
