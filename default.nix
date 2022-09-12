40bd65ba5b10f4b7c7767953a986f5e58c3f2d59
jleightcap <jack@leightcap.com>
2022-09-12 13:43:45 -0400

    FIX(jl): nix sha256

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
   pkgs = import (builtins.fetchTarball {
     name = "nixpkgs-unstable-arcz-fix-clash";
     url = "https://github.com/arcz/nixpkgs/archive/cad3ae120f5c9e4fd9c6cc2236f82c64b6f8fda8.tar.gz";
-    sha256 = "sha256:0wmjqx0yjp96fcdz9fjjfllb93kaxb9hzi9ikrzaapn2zdwrbl7a";
+    sha256 = "sha256:0rmiw11613y929h2qcxklb5zv7dzarwfa6m4qrjbzxz2nq1jk53q";
   }) {};
 
   clash = pkgs.haskellPackages.ghcWithPackages (p: with p; [
