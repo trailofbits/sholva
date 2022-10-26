86b097f598791bfea3174b6b8e03c86c0d7afcac
jleightcap <jack@leightcap.com>
2022-10-25 23:03:53 -0400

    TASK(jl): word sizes

--- src/Syscall/Recieve.hs
+++ src/Syscall/Recieve.hs
@@ -5,6 +5,7 @@ module Syscall.Recieve
     ) where
 
 import Clash.Prelude
+
 import Syscall.Internal
 
 syscallRecieveDFA :: SyscallDFA
@@ -14,14 +15,23 @@ syscallRecieveDFA (MkDFAState { eax = eax'
                               , ecx = ecx'
                               , state = SYSCALL_STATE_READ
                               })
-    -- recieve length zero; done.
-    | ecx' == 0 =
-        MkDFAState {eax = eax', ebx = ebx', ecx = 0, state = SYSCALL_STATE_DONE}
-    -- recieve length nonzero, incrememnt buffer pointer, decrement count.
+    -- step contains the last data to consume.
+    | ecx' < toEnum stepDataBytes =
+        MkDFAState
+            { eax = eax'
+            -- increment the pointer into memory by the remaining number bytes left to consume.
+            , ebx = ebx' + ecx'
+            -- zero the number of bytes left to consume.
+            , ecx = 0
+            , state = SYSCALL_STATE_DONE
+            }
+    -- more steps to consume.
     | otherwise =
         MkDFAState
             { eax = eax'
-            , ebx = ebx' + 1
-            , ecx = ecx' - 1
+            -- increment the pointer into memory.
+            , ebx = ebx' + toEnum stepPtrBytes
+            -- decrement the number of bytes left to consume.
+            , ecx = ecx' - toEnum stepDataBytes
             , state = SYSCALL_STATE_READ
             }
