912307bf4c262fb76983a3cf5f6993c80432c62d
jleightcap <jack@leightcap.com>
2022-11-01 16:53:00 -0400

    TEST(jl): nix, CI

--- default.nix
+++ default.nix
@@ -12,7 +12,19 @@ let
 in with pkgs; stdenv.mkDerivation {
   name = "sholva-dev";
 
-  nativeBuildInputs = [ clash verilog verilator ruby ];
+  nativeBuildInputs = [
+    cargo
+    clash
+    verilog verilator
+  ];
+  buildInputs = [
+    clippy
+    gdb
+    git
+    nasm
+    ruby
+    rustfmt
+  ];
 
   src = ./.;
 }
