267a2c3dfc2303bf768707ae5607d7b106225ee4
Lisa Overall <11458176+lisaoverall@users.noreply.github.com>
2022-12-15 14:36:13 -0800

    WIP test generation scripts
    Signed-off-by: Lisa Overall <lisa.overall@trailofbits.com>

--- test/buffer-overflow-gen-by-size.sh
+++ test/buffer-overflow-gen-by-size.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
+# HACK(jl): for this example, the trace lengths do not increase exponential below buffer sizes of ~1024 bytes.
+# we also want to demonstrate exponential behavior, but not spend exponentially more time tracing.
+# so, pick:
+# - a base _n_ s.t. n > 1... but not much bigger
+# - a shift  _k_ s.t. n^k > 1024... but not much bigger
 #
-# NOTE(jl): needs to be run from sholva's test directory.
-# returns: the IR0 artifacts in directory $2 for a buffer overflow of size $1
+# here, for n=1.3 k=28, exponential behavior is clear with a max/min of only ~10.
 #
-
+# ```python
+# >>> list(map(lambda n: round(1.3 ** (n + 28)), range(10)))
+# [1550, 2015, 2620, 3406, 4428, 5756, 7483, 9728, 12646, 16440]
+# ```
 n=$1
-CFLAGS="-fno-pic -g -m32 -mtune=i386 -Wl,-emain -nostdlib -static -std=c99 -mpreferred-stack-boundary=2 -fno-stack-protector -z execstack -DBUF_LEN=${n}" 
-gcc ${CFLAGS} buffer_overflow.c -o buffer_overflow"${n}".elf
-make buffer_overflow"${n}".trace.txt
-make buffer_overflow"${n}".circuit
+CFLAGS="-DBUF_LEN=${n}" make buffer_overflow.trace.txt -B
+mv buffer_overflow.trace.txt buffer_overflow"${n}".trace.txt
 
-# NOTE(jl): verify existance of directory.
-mkdir -p "$2"
-mkdir -p "$2"/instance
-mv buffer_overflow"${n}".public_input "$2"/instance/buffer_overflow"${n}".public_input
-mkdir -p "$2"/witness
-mv buffer_overflow"${n}".private_input "$2"/witness/buffer_overflow"${n}".private_input
-mkdir -p "$2"/relation
-mv buffer_overflow"${n}".circuit "$2"/relation/buffer_overflow"${n}".circuit
-
+make ${buffer_overflow"${n}"%.trace.txt}.circuit 
+# another step here to make .private_input, .public_input?
+mkdir -p ~/test-infrastructure/tests/buffer-overflow
+# mv buffer_overflow"${n}".{circuit,private_input,public_input} ~/test-infrastructure/tests/buffer-overflow
--- test/nop-gen-by-size.sh
+++ test/nop-gen-by-size.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+
 PRELUDE="""
 section .text
 global _start
+
 _start:
 """
 
@@ -10,24 +13,18 @@ mov eax, 1
 int 0x80
 """
 
-n=$1
-f=nop${n}
+n=$((2**$1))
+f=nop${n}.s
 
 { echo "${PRELUDE}";
-  for _ in $(seq "$n"); do
-  	echo "nop"
-  done
-  echo "${POSTLUDE}";
-} > "$f".s
+for _ in $(seq "$n"); do
+	echo "nop"
+done
+echo "${POSTLUDE}";
+} > "$f"
 
-make "$f".circuit
-
-# NOTE(jl): verify existence of directory.
-mkdir -p "$2"
-mkdir -p "$2"/instance
-mv ${f%.s}.public_input "$2"/instance/${f%.s}.public_input
-mkdir -p "$2"/witness
-mv ${f%.s}.private_input "$2"/witness/${f%.s}.private_input
-mkdir -p "$2"/relation
-mv ${f%.s}.circuit "$2"/relation/${f%.s}.circuit
-
+make ${f%.s}.trace.txt
+make ${f%.s}.circuit 
+# another step here?
+mkdir -p ~/test-infrastructure/tests/nop
+# Something like cp ${f%.s}.{circuit,private_input,public_input}
