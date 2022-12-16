162511f59ec63e2b08825612934cab8c2447f5f7
jleightcap <jack@leightcap.com>
2022-12-15 20:14:12 -0500

    NOTE(jl): 'atomic' `*-gen-by-size.sh` scripts.
    Signed-off-by: jleightcap <jack@leightcap.com>

--- test/nop-gen-by-size.sh
+++ test/nop-gen-by-size.sh
@@ -14,17 +14,17 @@ int 0x80
 """
 
 n=$((2**$1))
-f=nop${n}.s
+f=nop${n}
 
 { echo "${PRELUDE}";
-for _ in $(seq "$n"); do
-	echo "nop"
-done
-echo "${POSTLUDE}";
-} > "$f"
+  for _ in $(seq "$n"); do
+  	echo "nop"
+  done
+  echo "${POSTLUDE}";
+} > "$f".s
 
-make ${f%.s}.trace.txt
-make ${f%.s}.circuit 
-# another step here?
-mkdir -p ~/test-infrastructure/tests/nop
-# Something like cp ${f%.s}.{circuit,private_input,public_input}
+make "$f".circuit
+
+# NOTE(jl): verify existance of directory.
+mkdir -p $2
+cp nop"${n}".{circuit,circuit.{public,private}_input} $2
