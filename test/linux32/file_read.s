f9578b3f1e16a5fcc8fa3bb2bdb5040ee1d29ac3
Willy R. Vasquez <wrv@utexas.edu>
2023-02-25 10:38:18 -0600

    Update the test to compare read in values
    Signed-off-by: Willy R. Vasquez <wrv@utexas.edu>

--- test/linux32/file_read.s
+++ test/linux32/file_read.s
@@ -1,40 +1,72 @@
 section .text
 global _start
 
-; Read the contents from a file. 
-; Uses Linux syscalls
-; TODO: compare the read contents
+; Read the contents from a file using Linux syscalls
 
 _start:
   ; file open
-  mov eax, 2
-  mov ebx, filename
-  int 0x80
+  mov     eax, 2
+  mov     ebx, filename
+  int     0x80
 
   ; got fd, set to register
-  mov ebx, eax
+  mov     ebx, eax
 
   ; file read, fd in ebx
-  mov eax, 0
-  mov ecx, contents
-  mov edx, 11
-  int 0x80
+  mov     eax, 0
+  mov     ecx, contents
+  mov     edx, length
+  int     0x80
+
+  ; push the file descriptor 
+  push ebx
 
   ; compare contents 
+  mov     eax, expected_contents
+  mov     ebx, contents 
+  mov     ecx, length
+compare:
+  mov     edx, [eax]
+  mov     edi, [ebx]
+  cmp     edx, edi
+  jne     fail
 
+  ; increment the pointers
+  inc     eax
+  inc     ebx
+  ; decrement the counter
+  dec     ecx
+  cmp     ecx, 0
+  jne     compare
+
+success:
+  ; success
+  mov     edi, 0
+  jmp     done
+
+fail:
+  mov     edi, 1
+
+done:
+  ; get back file descriptor 
+  pop     ebx
 
   ; file close, fd in ebx
-  mov eax, 3
-  int 0x80
+  mov     eax, 3
+  int     0x80
 
   ; exit
   mov     eax, 60
   int     0x80
 
 section .data
-;filename: db "/home/user/file.txt", 0
-filename: db "file.txt", 0
-expected_contents: db "Hola Mundo", 0
+; Use this if we want the full path
+; Filepath limit is 255, imposed by OS
+;filename:          db "/home/user/file.txt", 0
+
+filename:           db "file.txt", 0
+expected_contents:  db "Hola Mundo", 0xa, 0
+length:             equ $ - expected_contents
 
 section .bss
-contents: resb 12
+contents:           resb 12
