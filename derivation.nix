31384e3392ef4d41137f1666692e36239937a7aa
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-02-28 00:11:14 -0500

    TEST(jl): CI stages passing locally
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

---
+++ derivation.nix
@@ -0,0 +1,19 @@
+{ sources ? import ../nix/sources.nix
+, pkgs ? import sources.nixpkgs { }
+}:
+
+let
+  # local derivations
+  mttn = pkgs.callPackage ./mttn/derivation.nix { };
+  tiny86 = pkgs.callPackage ./tiny86/derivation.nix { };
+in
+with pkgs; stdenv.mkDerivation {
+  name = "sholva";
+  src = ./.;
+
+  buildInputs = [
+    mttn
+    tiny86
+    git
+  ];
+}
