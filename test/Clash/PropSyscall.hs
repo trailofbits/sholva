f50377b5a3368e2b706d0eef52415283476eead8
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-03-08 10:56:10 -0500

    TEST(jl): validated `WRITE` preconditions
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

--- test/Clash/PropSyscall.hs
+++ test/Clash/PropSyscall.hs
@@ -8,34 +8,39 @@ import Syscall.Receive
 import Test.QuickCheck
 
 newtype ValidSyscallState =
-    ValidSyscallState SyscallState
-    deriving (Eq, Show)
+  ValidSyscallState SyscallState
+  deriving (Eq, Show)
 
 instance Arbitrary ValidSyscallState where
-    arbitrary = ValidSyscallState <$> elements [minBound .. maxBound]
+  arbitrary = ValidSyscallState <$> elements [minBound .. maxBound]
 
 newtype ValidSyscallDFAState =
-    ValidSyscallDFAState SyscallDFAState
-    deriving (Eq, Show)
+  ValidSyscallDFAState SyscallDFAState
+  deriving (Eq, Show)
 
 instance Arbitrary ValidSyscallDFAState where
-    arbitrary = do
-        eax' <- arbitrary
-        ecx' <- arbitrary
-        edx' <- arbitrary
-        (ValidSyscallState state') <- arbitrary
-        return $ ValidSyscallDFAState $ MkDFAState eax' ecx' edx' state'
+  arbitrary = do
+    eax' <- arbitrary
+    ecx' <- arbitrary
+    edx' <- arbitrary
+    (ValidSyscallState state') <- arbitrary
+    return $ ValidSyscallDFAState $ MkDFAState eax' ecx' edx' state'
 
 prop_syscall_receive_none :: ValidSyscallDFAState -> Property
 prop_syscall_receive_none (ValidSyscallDFAState s) =
-    (state s == SYSCALL_STATE_DONE) ==> syscallReceiveDFA s == s
+  (state s == SYSCALL_STATE_DONE) ==> syscallReceiveDFA s == s
 
 prop_syscall_receive_read :: ValidSyscallDFAState -> Property
 prop_syscall_receive_read (ValidSyscallDFAState s) =
-    (state s == SYSCALL_STATE_READ && ecx s > 0) ==>
-    let s' = syscallReceiveDFA s
-     in (eax s) == (eax s') &&
-        (ebx s) + 1 == (ebx s') && (ecx s) - 1 == (ecx s')
+  (state s == SYSCALL_STATE_WRITE && ecx s > toEnum stepDataBytes)
+  -- WRITE state with bytes remaining to recieve
+   ==>
+  let s' = syscallReceiveDFA s -- computed next state
+   in (eax s == eax s') -- %eax unchanged
+       &&
+      (ebx s + toEnum stepPtrBytes  == ebx s') -- pointer incremented
+      &&
+      (ecx s - toEnum stepDataBytes == ecx s') -- data count decremeneted
 
 -- QUICKCHECK
 return []
