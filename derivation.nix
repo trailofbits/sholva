4b937cfd24c5f4e86f15011c12f67eab4694e7af
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-04-14 15:30:47 -0400

    exploit: remove unnecessary unstable nixpkgs
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>
    
    exploit: remove unnecessary unstable nixpkgs
    
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>
    
    exploit: remove unnecessary unstable nixpkgs
    
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

--- derivation.nix
+++ derivation.nix
@@ -12,18 +12,27 @@ let
     sv_circuit = sv_circuit;
     verilog_tools = verilog_tools;
   };
-
   sholva-qemu =
     pkgs.callPackage ./runtime/qemu/qemu-i386.nix { sources = sources; };
-  sholva-jdk =
-    pkgs.callPackage ./runtime/openjdk/openjdk-llvm-i386.nix { pkgs = pkgs; };
+  sholva-jdk = pkgs.callPackage ./runtime/openjdk/openjdk-llvm-i386.nix {
+    sources = sources;
+  };
 in with pkgs;
 stdenv.mkDerivation {
   name = "sholva";
   src = ./.;
 
   buildInputs = [ nasm ];
-  propagatedBuildInputs = [ mttn tiny86 sholva-qemu python3 which sholva-jdk ];
+  propagatedBuildInputs = [
+    which
+    numactl
+    gdb
+    mttn
+    tiny86
+    sholva-qemu
+    python3
+    (enableDebugging pkgsi686Linux.jdk8_headless)
+  ];
 
   preCheck = ''
     patchShebangs ./test/
--- runtime/openjdk/default.nix
+++ runtime/openjdk/default.nix
@@ -1,3 +1,3 @@
-{ sources ? import ../../nix/sources.nix, pkgs ? import sources.nixpkgs-unstable { } }:
+{ sources ? import ../../nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
 
-pkgs.callPackage ./openjdk-llvm-i386.nix { }
+pkgs.callPackage ./openjdk-llvm-i386.nix { sources = sources; }
