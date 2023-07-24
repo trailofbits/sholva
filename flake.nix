deb0a9c57097336775b58d390d3c49dc633b6df2
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-07-24 08:37:49 -0400

    test: tiny86 tests isolated
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

--- flake.nix
+++ flake.nix
@@ -1,120 +1,133 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    clash.url = "github:clash-lang/clash-compiler";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    sv_circuit = {
-      url = "github:trailofbits/sv_circuit";
-      flake = false;
+    clash = {
+      url = "github:clash-lang/clash-compiler";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sv_circuit = { url = "github:trailofbits/sv_circuit?ref=jl/nix-flake"; };
     verilog_tools = {
-      url = "github:trailofbits/verilog_tools";
-      flake = false;
+      url = "github:trailofbits/verilog_tools?ref=jl/nix-flake";
+    };
+    wiztoolkit = {
+      url = "git+ssh://git@github.mit.edu/sieve-all/wiztoolkit.git?ref=jl/nix";
     };
   };
 
-  nixConfig = {
-    extra-substituters = [ ];
-    extra-trusted-public-keys = [ ];
-  };
+  outputs = { self, clash, nixpkgs, flake-compat, rust-overlay, sv_circuit
+    , verilog_tools, ... }:
+    let
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          inherit (pkgs) stdenv lib;
 
-  outputs = { self, clash, nixpkgs, flake-compat, flake-utils, rust-overlay
-    , sv_circuit, verilog_tools, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages = rec {
-          default = sholva;
+          # add dissasembly support via capstone.
+          sholva-qemu = pkgs.qemu.overrideAttrs (old: rec {
+            configureFlags = [
+              "--disable-strip"
+              "--enable-capstone"
+              "--enable-tools"
+              "--localstatedir=/var"
+              "--meson=meson"
+              "--sysconfdir=/etc"
+              "--target-list=i386-linux-user"
+            ];
+            buildInputs = old.buildInputs ++ (with pkgs; [ capstone ]);
+          });
 
-          sholva-qemu = with import nixpkgs { inherit system; };
-            qemu.overrideAttrs (old: rec {
-              configureFlags = [
-                "--disable-strip"
-                "--enable-capstone"
-                "--enable-tools"
-                "--localstatedir=/var"
-                "--meson=meson"
-                "--sysconfdir=/etc"
-                "--target-list=i386-linux-user"
-              ];
-              buildInputs = old.buildInputs ++ (with pkgs; [ capstone ]);
-            });
-
-          mttn = with import nixpkgs {
-            inherit system;
-            overlays = [ (import rust-overlay) ];
-          };
+          # https://nixos.wiki/wiki/Overlays, "In a Nix flake"
+          mttn = with pkgs.extend (import rust-overlay);
             rustPlatform.buildRustPackage rec {
               name = "mttn";
               src = ./mttn;
               buildInputs = [ rust-bin.beta.latest.default ];
               cargoLock = { lockFile = ./mttn/Cargo.lock; };
-              doCheck =
-                false; # FIXME(jl): determine best design for `mttn`'s tracing tests.
-              meta = with lib; {
-                description = "Tiny86 tracer";
-                license = licenses.agpl3Only;
-                platforms = [ "x86_64-linux" "i686-linux" ];
-              };
-            };
 
-          tiny86 = with import nixpkgs { inherit system; };
-            stdenv.mkDerivation {
-              name = "tiny86";
-              src = ./tiny86;
-              buildInputs = with clash.packages.${system};
-                [ clash-ghc clash-prelude ]
-                ++ [ (import sv_circuit) (import verilog_tools) ]
-                ++ [ nasm ruby python3 verible verilator verilog ];
-
-              # a small wrapper to expose the synthesized circuit through a script in the derivation output
-              installPhase = ''
-                set -e
-
-                mkdir -p $out/circuit
-                mv tiny86.blif $out/circuit
-
-                mkdir -p $out/bin
-                cat << EOF >> $out/bin/tiny86.sh
-                #!/bin/sh
-                cat $out/circuit/tiny86.blif
-                EOF
-                chmod +x $out/bin/tiny86.sh
+              nativeCheckInputs = [ nasm ];
+              # NOTE: mttn tests use example binaries to verify reproducibility.
+              preCheck = ''
+                make -C test elfs
               '';
-              meta = with lib; {
-                description = "Tiny86 transition circuit";
-                license = licenses.agpl3Only;
-              };
-            };
+              doCheck = true;
 
-          sholva = with import nixpkgs { inherit system; };
-            stdenv.mkDerivation {
-              name = "sholva";
-              src = ./test;
-
-              buildInputs = [ sholva-qemu mttn nasm tiny86 ];
-
-              doBuild = false;
-
-              preCheck = "patchShebangs ./test/";
-              doCheck = false;
-
-              installPhase = ''
-                mkdir -p $out/ir0
-                install -t  $out/ir0 *.circuit
+              # In addition to the mttn binary,
+              # Package all traced programs into derivation output.
+              postInstall = ''
+                MTTN=$out/bin/mttn RUST_LOG=info make -C test traces
+                install -Dt $out/traces test/*.trace.txt
               '';
-
-              meta = with lib; {
-                description =
-                  "Zero-knowledge proofs for i386 program execution";
-                license = licenses.agpl3Only;
-              };
             };
+
+          tiny86 = stdenv.mkDerivation {
+            name = "tiny86";
+            src = ./tiny86;
+            buildInputs = with clash.packages.${system};
+              [ clash-ghc clash-prelude ] ++ [
+                sv_circuit.packages.${system}.sv_circuit
+                verilog_tools.packages.${system}.verilog_tools
+              ] ++ (with pkgs; [ nasm ruby python3 verible verilator verilog ]);
+
+            preInstall = "patchShebangs codegen/";
+            installPhase = ''
+              set -e
+
+              #mkdir -p $out/circuit
+              #cp tiny86.blif $out/circuit/
+
+              mkdir -p $out/bin
+              cp codegen/* $out/bin/
+            '';
+          };
+
+          buildInputs = [ mttn tiny86 ];
+
+        in rec {
+          default = sholva;
+          sholva = stdenv.mkDerivation {
+            name = "sholva";
+            src = ./test;
+
+            inherit buildInputs;
+            doBuild = false;
+
+            preCheck = ''
+              cp ${mttn}/traces/*.trace.txt .
+            '';
+            doCheck = true;
+
+            installPhase = ''
+              mkdir -p $out/ir0
+              install -t  $out/ir0 *.circuit
+            '';
+
+            meta = with lib; {
+              description = "Zero-knowledge proofs for i386 program execution";
+              license = licenses.agpl3Only;
+            };
+          };
+
+          inherit mttn tiny86;
+        });
+
+      apps = forAllSystems (system: {
+        mttn = {
+          type = "app";
+          program = "${self.packages.${system}.mttn}/bin/mttn";
         };
       });
+    };
 }
