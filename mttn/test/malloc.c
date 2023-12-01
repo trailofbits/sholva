c9ee48c4cb6a75e628cc08e66736f19a42486d8c
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-12-01 07:53:18 -0500

    mttn: brk model
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

---
+++ mttn/test/malloc.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+
+int main(int ac, char *av[]) {
+    void* p = malloc(1);
+    return 0;
+}
---
+++ mttn/test/malloc.gdb
@@ -0,0 +1,4 @@
+file malloc.elf
+source stop-at-main.gdb
+source print-pid.gdb
+detach
